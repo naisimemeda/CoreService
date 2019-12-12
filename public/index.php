7dd7b04175ad997ab32f255de960d85a9185621a
naisimemeda <2514430140@qq.com>
2019-12-12 22:17:52 +0800

    facade

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(App\Container\Http\Kernel::class);
+
 $response = $kernel->handle();
 
 Auth::Login();
